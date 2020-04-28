56a2674f5a6dfe0b3c439cb97ea35ae34727f146
Igor <igormcsouza@gmail.com>
2020-04-28 15:36:19 -0300

    app

---
+++ app/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasklist.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_completed = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        task_completed = request.form['completed']
+
+        new_task = Todo(content=task_content, date_completed=task_completed)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    delete_task = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting it"
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['content']
+        task.date_completed = request.form['completed']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Issue updating the Task"
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
